be4f929615167ad669029daf65b8dab62eef66b1
jyb08 <jiye080202@gmail.com>
2022-11-18 18:18:58 +0300

    ---

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("------------------------------")
+
+
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+
+function draw(){
+    background(200);
+    fill(255, 0, 0, 255);
+    rect(50, 50, 100, 200);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
